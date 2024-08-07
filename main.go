c5dd8829c61fd1829bc0b2e3c6ea5d0a3d3c849e
Dornam0930 <warhammer925@yahoo.com>
2024-08-07 13:00:52 -0700

    initial user I/O added

--- main.go
+++ main.go
@@ -1,9 +1,53 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
+type keywords struct {
+	name  string
+	usage string
+	//callback func() error
+}
+
 func main() {
-	fmt.Println("hello world")
+	commands := map[string]keywords{
+		"help": {
+			name:  "help",
+			usage: "displays a help message",
+			//callback: commandHelp,
+		},
+		"exit": {
+			name:  "exit",
+			usage: "exits the pokedex",
+			//callback: commandExit,
+		},
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("pokedex > ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Errorf("Error: %v", err)
+			continue
+		}
+		input = strings.TrimRight(input, "\n")
+		if input == commands["help"].name {
+			fmt.Println("--------------------")
+			fmt.Println("Pokedex help:")
+			fmt.Println("")
+			fmt.Println("Commands:")
+			fmt.Printf("%v: %v\n", commands["help"].name, commands["help"].usage)
+			fmt.Printf("%v: %v\n", commands["exit"].name, commands["exit"].usage)
+			fmt.Println("--------------------")
+		}
+		if input == commands["exit"].name {
+			os.Exit(0)
+		}
+	}
 }
